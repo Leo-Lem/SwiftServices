afb77b0961a39723d633464ccd27027c87041686
Leopold Lemmermann <leopold@lemmermann.com>
2022-11-23 19:21:10 +0100

    Added a static mock property.

--- Sources/IndexingService/mock.swift
+++ Sources/IndexingService/mock.swift
@@ -1,8 +1,12 @@
 //	Created by Leopold Lemmermann on 20.10.22.
 
+public extension IndexingService where Self == MockIndexingService {
+  static var mock: MockIndexingService { MockIndexingService() }
+}
+
 open class MockIndexingService: IndexingService {
   public init() {}
-  
+
   public func updateReference<T: Indexable>(to indexable: T) {
     print("Created reference to \(indexable.getTitle() ?? "")!")
   }
