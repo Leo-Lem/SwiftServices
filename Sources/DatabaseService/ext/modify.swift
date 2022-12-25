e21a5f1783506c4af6c2a701058193617cf32976
Leopold Lemmermann <leopold@lemmermann.com>
2022-12-25 19:59:03 +0100

    Added a modify method with a key path and a new value.

--- Sources/DatabaseService/ext/modify.swift
+++ Sources/DatabaseService/ext/modify.swift
@@ -17,4 +17,18 @@ public extension DatabaseService {
       return convertible
     } else { throw DatabaseError.doesNotExist(T.self, id: id) }
   }
+  
+  /// Modifies the database object in the provided way
+  /// - Parameters:
+  ///   - type: The type of the ``DatabaseObjectConvertible``.
+  ///   - id: The id of the ``DatabaseObjectConvertible``.
+  ///   - property: The key path of the modified property.
+  ///   - newValue: The new value.
+  /// - Returns: A modified ``DatabaseObjectConvertible``.
+  @discardableResult
+  func modify<T: DatabaseObjectConvertible, U>(
+    _ type: T.Type = T.self, with id: T.ID, property: WritableKeyPath<T, U>, newValue: U
+  ) async throws -> T {
+    try await modify(with: id) { $0[keyPath: property] = newValue }
+  }
 }
